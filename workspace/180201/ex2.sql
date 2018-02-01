a312073cfe81a9f7009b328967022f0c2a993aff
steve park <32486591+stevefordev@users.noreply.github.com>
2018-02-01 19:47:23 +0900

    Update ex2.sql

--- workspace/180201/ex2.sql
+++ workspace/180201/ex2.sql
@@ -13,6 +13,6 @@ FROM employees a, departments b
 WHERE 
 a.department_id = b.department_id
 AND TO_CHAR(hire_date, 'YYYY') < 2005
-AND salary <= (SELECT AVG(salary) 
+AND (salary * 12) <= (SELECT AVG(salary * 12) 
                FROM employees)
 ORDER BY 6
