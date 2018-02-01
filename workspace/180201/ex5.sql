ee93fdf81881ace2fa7fb3eb450c38eefd28d6d7
steve park <32486591+stevefordev@users.noreply.github.com>
2018-02-01 18:54:24 +0900

    Update ex5.sql

--- workspace/180201/ex5.sql
+++ workspace/180201/ex5.sql
@@ -4,10 +4,9 @@ SELECT a.employee_id 사번,
        CONCAT(CONCAT(a.first_name, ' '), a.last_name) 사원성명,
        c.job_title 직업명,
        CONCAT(CONCAT(d.first_name,' '), d.last_name) 상사명
-FROM employees a, departments b, jobs c, employees d
-WHERE a.department_id = b.department_id
-AND b.department_name = 'Finance'
+FROM employees a, employees d, jobs c
+WHERE
+a.department_id = (SELECT department_id FROM departments WHERE department_name = 'Finance')
 AND a.job_id = c.job_id
 AND a.manager_id = d.employee_id
-
 
