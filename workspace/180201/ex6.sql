5ba67bde081e25406ca9ac9c81a40e2354c229ad
steve park <32486591+stevefordev@users.noreply.github.com>
2018-02-02 09:28:44 +0900

    Update ex6.sql

--- workspace/180201/ex6.sql
+++ workspace/180201/ex6.sql
@@ -3,7 +3,7 @@
 SELECT employee_id 사번, 
        CONCAT(CONCAT(first_name, ' '), last_name) 이름,
        TO_CHAR(salary * 12, '$999,999,999') 본봉,
-       DECODE(commission_pct, null, '0%', CONCAT(TO_CHAR(commission_pct, '.00'),'%')) 성과급비율,
+       DECODE(commission_pct, null, '0%', CONCAT(ROUND(commission_pct, 2),'%')) 성과급비율,
        TO_CHAR(salary * 12 * (1 +  NVL(commission_pct,0)), '$999,999,999') 실수령액,
        CONCAT( ROUND((sysdate - hire_date) / 365), '년차') 경력   
 FROM employees
