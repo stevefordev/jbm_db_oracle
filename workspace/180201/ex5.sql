6f8d59af77e946e7a690a934ace60271c32d3232
steve park <32486591+stevefordev@users.noreply.github.com>
2018-02-02 11:05:07 +0900

    Update ex5.sql

--- workspace/180201/ex5.sql
+++ workspace/180201/ex5.sql
@@ -10,3 +10,14 @@ a.department_id = (SELECT department_id FROM departments WHERE department_name =
 AND a.job_id = c.job_id
 AND a.manager_id = d.employee_id
 
+
+
+SELECT a.employee_id 사번,
+      a.first_name ||' '|| a.last_name 사원성명,
+       c.job_title 직업명,
+      d.first_name ||' '|| d.last_name 상사명
+FROM employees a, employees d, jobs c
+WHERE
+a.department_id = (SELECT department_id FROM departments WHERE department_name = 'Finance')
+AND a.job_id = c.job_id
+AND a.manager_id = d.employee_id
